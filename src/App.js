2993a67940c8884c0ce95cfed3da88989d2170b6
lwtz <w.h@outlook.com.br>
2021-08-11 00:13:27 -0300

    Finish Course

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
+import RegistrationForm          from './components/RegistrationForm/RegistrationForm'
+import { Container, Typography } from '@material-ui/core'
 import './App.css'
-import RegistrationForm from './components/RegistrationForm/RegistrationForm'
-import { Container, Typography }    from '@material-ui/core'
 import 'fontsource-roboto'
 
-function App() {
+
+function App(){
+    function sendForm(data){
+        console.log(data)
+    }
+
+    function validateCPF(cpf){
+        const regexCPF = new RegExp(/^[0-9]{3}.?[0-9]{3}.?[0-9]{3}-?[0-9]{2}/)
+        if( !regexCPF.test(cpf) || cpf.length !==  11 ){
+            return {valid: false, text: 'CPF INVÁLIDO'}
+        } else {
+            return {valid: true, text: ''}
+        }
+    }
+
     return (
         <Container maxWidth="sm" component={ 'article' }>
-            <Typography variant={'h3'} align={'center'} component="h1">Registration Form</Typography>
-            <RegistrationForm />
+            <Typography
+                variant={ 'h3' }
+                align={ 'center' }
+                component="h1">Registration Form</Typography>
+            <RegistrationForm
+                toSend={ sendForm }
+                validarCPF={ validateCPF }
+            />
         </Container>
     )
 }
--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -1,22 +1,75 @@
-import React                                           from 'react'
+import React, { useState }                             from 'react'
 import { Button, TextField, Switch, FormControlLabel } from '@material-ui/core'
 
-// import TextField  from '@material-ui/core/TextField'
 
+function RegistrationForm({toSend, validarCPF}){
+    const [ name, setName ] = useState('')
+    const [ lastname, setLastName ] = useState('')
+    const [ cpf, setCPF ] = useState('')
+    const [ promotions, setPromotions ] = useState(true)
+    const [ news, setNews ] = useState(true)
+    const [ error, setError ] = useState({cpf: {valid: true, text: ''}})
 
-function RegistrationForm() {
     return (
-        <form>
-            <TextField id="name" label="Name" variant="outlined" autoFocus={ true } fullWidth margin={ 'normal' } />
-            <TextField id="lastname" label="LastName" variant="outlined" fullWidth margin={ 'normal' } />
-            <TextField id="cpf" label="CPF" variant="outlined" fullWidth margin={ 'normal' } />
+        <form onSubmit={ (event) => {
+            event.preventDefault()
+            toSend({name, lastname, cpf, promotions, news})
+        }
+        }>
+            <TextField
+                value={ name }
+                onChange={ (event) => {
+                    setName(event.target.value)
+                } }
+                id="name"
+                label="Name"
+                variant="outlined"
+                autoFocus={ true }
+                fullWidth
+                margin={ 'normal' }
+            />
+            <TextField
+                value={ lastname }
+                onChange={ (event) => {
+                    setLastName(event.target.value)
+                } }
+                id="lastname"
+                label="LastName"
+                variant="outlined"
+                fullWidth
+                margin={ 'normal' }
+            />
+            <TextField
+                value={ cpf }
+                inputProps={{ maxLength: 11 }}
+                error={ !error.cpf.valid }
+                helperText={ error.cpf.text }
+                onChange={ (event) => {
+                    setCPF(event.target.value)
+                } }
+                onBlur={ (event) => {
+                    const valid = validarCPF(cpf)
+                    setError({cpf: valid})
+                } }
+                id="cpf"
+                label="CPF"
+                variant="outlined"
+                fullWidth
+                margin={ 'normal' }
+            />
 
             <FormControlLabel
+                // checked={promotions}
                 control={
                     <Switch
-                        name="Promotions"
+                        onChange={ (event) => {
+                            setPromotions(event.target.checked)
+                        }
+                        }
+                        name="promotions"
                         color="primary"
-                        defaultChecked={ true }
+                        checked={ promotions }
+                        // defaultChecked={ promotions }
                     />
                 }
                 label="Promotions"
@@ -26,9 +79,13 @@ function RegistrationForm() {
             <FormControlLabel
                 control={
                     <Switch
-                        name="News"
+                        onChange={ (event) => {
+                            setNews(event.target.checked)
+                        }
+                        }
+                        name="news"
                         color="primary"
-                        defaultChecked={ true }
+                        checked={ news }
                     />
                 }
                 label="News"
